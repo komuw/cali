8a959525425fc41b3255b475b7b64c3b2dbb29d0
komuw <komuw05@gmail.com>
2018-08-11 23:08:37 +0300

    success

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,8 +1,6 @@
 package lexer
 
 import (
-	"fmt"
-
 	"github.com/komuw/khaled/token"
 )
 
@@ -13,111 +11,141 @@ It will take source code as input and output the tokens that rep the source code
 */
 
 type Lexer struct {
-	Input      string
-	Ch         byte   // current char under examination
-	Position   int    // position of Ch
-	ByteStream []byte // value of Input as bytes
-	// readPosition int  // current reading position in input (after current char)
+	input        string
+	ch           byte   // current char under examination
+	position     int    // position of Ch
+	readPosition int    //  next reading position in input (after current char)
+	ByteStream   []byte // value of Input as bytes
 
 }
 
 func NewLexer(input string) *Lexer {
-	l := &Lexer{Input: input}
-	for index := 0; index < len(l.Input); index++ {
-		l.ByteStream = append(l.ByteStream, l.Input[index])
+	l := &Lexer{input: input}
+	for index := 0; index < len(l.input); index++ {
+		l.ByteStream = append(l.ByteStream, l.input[index])
 
 	}
+
+	/*
+		use readChar, so our *Lexer is in a fully working state b4 anyone calls NextToken()
+	*/
+	l.readChar()
 	return l
-}
-
-// skipWhiteSpace eats whitespace, because it is of no use to khaled unlike python
-func (l *Lexer) skipWhiteSpace() {
-	if l.Ch == ' ' || l.Ch == '\t' || l.Ch == '\n' || l.Ch == '\r' {
-		// skip that char
-		l.Position = l.Position + 1
-		l.Ch = l.Input[l.Position]
-	}
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
-	l.skipWhiteSpace()
+	l.skipWhitespace()
 
 	/*
-		TODO: tunr this to func
-		the lexer only supports ASCII characters instead of full Unicode. This lets us keep things simple.
-		To support Unicode/UTF-8 we would need to change l.Ch from a byte to rune and change the way we read the
-		next characters, since they could be multiple bytes wide now. Using l.Input[l.readPosition]wouldn’t work anymore
+		We look at the current character under
+		examination (l.ch) and return a token depending on which character it is.
 	*/
-	if l.Position >= len(l.Input) {
-		l.Ch = 0
-	} else {
-		l.Ch = l.Input[l.Position]
-	}
-	fmt.Println("l.Ch", string(l.Ch))
-
-	// read current character; we'll advance at end
-	switch l.Ch {
+	switch l.ch {
 	case '=':
-		tok = token.NewToken(token.ASSIGN, l.Ch)
+		tok = token.NewToken(token.ASSIGN, l.ch)
 	case ';':
-		tok = token.NewToken(token.SEMICOLON, l.Ch)
+		tok = token.NewToken(token.SEMICOLON, l.ch)
 	case '(':
-		tok = token.NewToken(token.LPAREN, l.Ch)
+		tok = token.NewToken(token.LPAREN, l.ch)
 	case ')':
-		tok = token.NewToken(token.RPAREN, l.Ch)
+		tok = token.NewToken(token.RPAREN, l.ch)
 	case ',':
-		tok = token.NewToken(token.COMMA, l.Ch)
+		tok = token.NewToken(token.COMMA, l.ch)
 	case '+':
-		tok = token.NewToken(token.PLUS, l.Ch)
+		tok = token.NewToken(token.PLUS, l.ch)
 	case '{':
-		tok = token.NewToken(token.LBRACE, l.Ch)
+		tok = token.NewToken(token.LBRACE, l.ch)
 	case '}':
-		tok = token.NewToken(token.RBRACE, l.Ch)
+		tok = token.NewToken(token.RBRACE, l.ch)
 	case 0: // ASCII code for "NUL"
 		tok.Value = ""
 		tok.Type = token.EOF
-		// default:
-		// 	if isLetter(l.Ch) {
-		// 		/*
-		// 		 if we encounter a letter; read until we encounter a non letter(ie read whole word)
-		// 		 eg if we encounter- let - then we need to read whole of it(let) as one token
-		// 		*/
-		// 		tok.Value = l.readIdentifier()
-		// 		tok.Type = token.LookupIdent(tok.Value)
-		// 		return tok
-		// 	} else {
-		// 		tok = token.NewToken(token.ILLEGAL, l.Ch)
-		// 	}
+	default:
+		/*
+			Our lexer needs to  recognize whether the current character is a letter and if so,
+			it needs to read the rest of the identifier/keyword until it encounters a non-letter-character.
+		*/
+		if isLetter(l.ch) {
+			/*
+			 if we encounter a letter; read until we encounter a non letter(ie read whole word)
+			 eg if we encounter- let - then we need to read whole of it(let) as one token
+			*/
+			tok.Value = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Value)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Value = l.readNumber()
+			return tok
+		} else {
+			tok = token.NewToken(token.ILLEGAL, l.ch)
+		}
+
 	}
 
-	// advance
-	if l.Position < len(l.Input) {
-		l.Position = l.Position + 1
-	}
+	/*
+		Before returning the token we advance our pointers into the input
+		so when we call NextToken() again the l.ch field is already updated
+	*/
+	l.readChar()
 	return tok
 }
 
-func (l *Lexer) readIdentifier() string {
-	wordStartPos := l.Position
-	for isLetter(l.Ch) {
-		l.Ch = l.Input[l.Position]
-		l.Position = l.Position + 1
+/* readChar gives us the next character and advance our position in the input string.
+the lexer only supports ASCII characters instead of full Unicode. This lets us keep things simple.
+To support Unicode/UTF-8 we would need to change l.ch from a byte to rune and change the way we read the
+next characters, since they could be multiple bytes wide now. Using l.input[l.readPosition]wouldn’t work anymore
+*/
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
 	}
-	fmt.Println("l.Input[wordStartPos:l.Position]", string(l.Input[wordStartPos:l.Position]))
-	return l.Input[wordStartPos:l.Position]
+	l.position = l.readPosition
+	l.readPosition += 1
 }
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
 func isLetter(ch byte) bool {
+	/*
+		In khaled we treat _ as a letter and allow it in identifiers and keywords.
+		That means we can have var names like foo_bar. If you want to allow other chars in ua lang; add them here.
+	*/
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
-// func (l *Lexer) readNumber() string {
-// 	position := l.Position
-// 	for isDigit(l.Ch) {
-// 		l.readChar()
-// 	}
-// 	return l.Input[position:l.Position]
-// }
-// func isDigit(ch byte) bool {
-// 	return '0' <= ch && ch <= '9'
-// }
+/*
+eat whitespace. khaled doesnt require it unlike python.
+This func is found in a lot of parsers. Sometimes it’s called eatWhitespace/consumeWhitespace.
+Which chars these functions actually skip depends on the language being lexed.
+*/
+func (l *Lexer) skipWhitespace() {
+
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+/*
+We only read intergers. What about floats,hex notation, Octal notation?
+We ignore em and say khaled doesn't support them.
+*/
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
