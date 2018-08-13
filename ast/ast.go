d0082abab48ce1a80ed48a5b9c84433625ea2189
komuw <komuw05@gmail.com>
2018-08-13 22:12:22 +0300

    add parsing for let statements in a program like let x = 5; we add parsing for LET but we are currently not parsing for example the int(5)

---
+++ ast/ast.go
@@ -0,0 +1,97 @@
+package ast
+
+import "github.com/komuw/khaled/token"
+
+/*
+ Every node in our AST has to implement the Node interface.
+ TokenValue() is only used for debugging.
+*/
+type Node interface {
+	TokenValue() string
+}
+
+type Statement interface {
+	Node
+	statementNode() // dummy method; used to guide compiler to differentiate Statement and Expression interfaces
+}
+
+type Expression interface {
+	Node
+	expressionNode() //dummy method
+}
+
+/*
+Program implements the Node interface.
+Program node will be the root node of any AST produced by the parser.
+Every valid program is a series/slice of statements
+*/
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenValue() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenValue()
+	} else {
+		return ""
+	}
+}
+
+/*
+LetStatement implements the Statement interface
+
+Look at an example like;
+	let x = 5;
+The fields its(AST) should have are;
+1. one for let token
+2. another for name of identifier(x)
+3. finally one for the expression(5)
+
+Example of LetStatement struct for; let x = 5;
+	&ast.LetStatement{
+		Token: token.Token{Type: "LET", Value: "let"},
+		Name: &ast.Identifier{
+			Token: token.Token{Type: "IDENT", Value: "x", }, Value: "x"},
+		Value: nil,
+	}
+*/
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenValue() string {
+	return ls.Token.Value
+}
+
+/*
+Identifier implements the Expression interface
+
+It has fields;
+1. one for the identifer(x) token
+2. one for the expression(5)
+
+Hower in `let x = 5;` 5 is not an expression it is a statement ****
+TODO: come back here.
+*/
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string      //
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Value }
+
+/*
+let x = 5;
+can be represented by an AST like;
+
+					*ast.Program
+					Statements
+						|
+					*ast.LetStatement
+	*ast.Identifier <--  Name
+						Value --> *ast.Expression
+*/
---
+++ parser/parser_test.go
@@ -0,0 +1,87 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/komuw/khaled/ast"
+	"github.com/komuw/khaled/lexer"
+)
+
+func TestLetStatementsSimple(t *testing.T) {
+	input := `
+let x = 5;
+`
+	l := lexer.NewLexer(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 1 {
+		t.Fatalf("\n No of program.Statements. \ngot %#+v \nwanted %#+v", len(program.Statements), 1)
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+func TestLetStatementsExtended(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.NewLexer(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("\n No of program.Statements. \ngot %#+v \nwanted %#+v", len(program.Statements), 3)
+	}
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenValue() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenValue())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s",
+			name, letStmt.Name.TokenLiteral())
+		return false
+	}
+	return true
+}
