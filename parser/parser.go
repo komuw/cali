8a51d44f06466163e2a8650fe2732e1e06f1e4d9
komuW <komuw05@gmail.com>
2018-08-14 16:09:52 +0300

    add parseError: parser error handling

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/komuw/khaled/ast"
 	"github.com/komuw/khaled/lexer"
 	"github.com/komuw/khaled/token"
@@ -16,20 +18,39 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func NewParser(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{l: l, errors: []string{}}
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 	return p
 }
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+func (p *Parser) peekError(t token.TokenType) {
+	// TODO: add line numbers to this errors
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken
 	p.peekToken = p.l.NextToken()
 }
 
+/*
+ParseProgram :
+1. constructs the root node of the AST, an *ast.Program
+2. It iterates over every token in input until an token.EOF by repeatedly calling nextToken()
+   nextToken() advances both p.curToken and p.peekToken
+3. In every iteration it calls parseStatement, whose job it is to parse a statement.
+If parseStatement returned something(not nil), its return value is added to Statements slice
+4. When nothing is left to parse the *ast.Program root node is returned.
+*/
 func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
 	program.Statements = []ast.Statement{}
@@ -57,17 +78,28 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 }
 
+/*
+parseLetStatement:
+1. constructs an *ast.LetStatement node with the current token(token.LET)
+2. advances the tokens while making assertions about the next token with calls to expectPeek.
+  - First it expects a token.IDENT, which it then uses to construct an *ast.Identifier node
+  - Then it expects an equal sign and finally it SKIPS over the expression following the equal sign until it encounters a semicolon.
+  The skipping of expressions will be replaced, of course, as soon as we know how to parse expressions.
+*/
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 	if !p.expectPeek(token.IDENT) {
+		/*
+			if we get an UNexpected next token, we should raise a parseError(eg let = 5)
+		*/
 		return nil
 	}
 	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Value}
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
-	// TODO: We're skipping the expressions until we
-	// encounter a semicolon
+	// TODO: SKIP parsing expressions until we know how to parse them.
+	// Continue until we encounter a semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
@@ -78,11 +110,17 @@ func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
 }
 
+/*
+expectPeek() method is one of the "assertion funcs" that all parsers have.
+They enforce the correctness of the order of tokens by checking the type of the next token.
+Only if the type is correct does it advance the tokens by calling nextToken.
+*/
 func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,13 +7,19 @@ import (
 	"github.com/komuw/khaled/lexer"
 )
 
+/*
+We are providing source code as input instead of tokens,
+since that makes the tests much more readable and understandable.
+Ofcourse this means that bugs in the lexer would lead to failures of parser tests; coupling.
+*/
 func TestLetStatementsSimple(t *testing.T) {
 	input := `
-let x = 5;
-`
+		let x = 5;
+		`
 	l := lexer.NewLexer(input)
 	p := NewParser(l)
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -35,13 +41,14 @@ let x = 5;
 }
 func TestLetStatementsExtended(t *testing.T) {
 	input := `
-let x = 5;
-let y = 10;
-let foobar = 838383;
-`
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;
+		`
 	l := lexer.NewLexer(input)
 	p := NewParser(l)
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -64,7 +71,41 @@ let foobar = 838383;
 	}
 }
 
+func TestLetStatementsParseError(t *testing.T) {
+	input := `
+		let = 5;
+		`
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+	_ = p.ParseProgram()
+
+	errors := p.Errors()
+	if len(errors) == 0 {
+		t.Fatalf("\n No errors found. \ngot %#+v \nwanted %#+v", len(errors), 1)
+	}
+	t.Logf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Logf("parser error: %q", msg)
+	}
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	/*
+		We get an AST(s argument) like;
+			&ast.LetStatement{
+				Token: token.Token{Type: "LET", Value: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: "IDENT", Value: "x"},
+					Value: "x"},
+				Value: nil,
+				}
+
+		In this test helper func we are not checking s.Value
+		This is because, that value is currently nil(instead of 5 for input like let x = 5;)
+		It is nil because the only thing our parser currently is doing is only parsing let statements.
+		It doesnt yet know how to parse expressions(the thing after the equal sign in let x = 5;)
+		see documentation for parseLetStatement func.
+	*/
 	if s.TokenValue() != "let" {
 		t.Errorf("s.TokenValue not 'let'. got=%q", s.TokenValue())
 		return false
@@ -84,4 +125,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
